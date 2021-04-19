adece9aeeac260319057f16090483b1da1791149
tonyw0527 <tonyw0527@gmail.com>
2021-04-20 05:05:31 +0900

    Update cheatSheet - add Dynamic Programming

--- cheatSheet.py
+++ cheatSheet.py
@@ -24,7 +24,7 @@ def recursive_function(n):
 
 recursive_function(1)
 
-# DFS - stack 자료구조를 기반으로함. -> 따라서 재귀 함수를 이용하여 간결하게 구현.
+# Depth First Search - stack 자료구조를 기반으로함. -> 따라서 재귀 함수를 이용하여 간결하게 구현.
 
 
 def dfs(graph, v, visited):
@@ -53,7 +53,7 @@ visited = [False] * 9  # 노드의 방문 정보. 노드의 개수는 8개이다
 
 dfs(graph, 1, visited)
 
-# BFS - queue 자료구조 기반. O(N)의 시간 복잡도. 일반적인 경우 실제 수행 시간은 DFS보다 좋은 편.
+# Breadth First Search - queue 자료구조 기반. O(N)의 시간 복잡도. 일반적인 경우 실제 수행 시간은 DFS보다 좋은 편.
 
 
 def bfs(graph, start, visited):
@@ -88,3 +88,52 @@ graph = [  # 노드의 연결 정보. 연결 여부만 나타낼뿐 거리를 �
 visited = [False] * 9  # 노드의 방문 정보. 노드의 개수는 8개이다.
 
 bfs(graph, 1, visited)
+
+# Dynamic Programming
+
+# 피보나치 함수(Fibonacci Function)을 재귀함수로 구현 -> O(2^N)의 지수 시간 복잡도...
+
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    return fibo(x - 1) + fibo(x - 2)
+
+
+print(fibo(4))
+
+# 탑다운(재귀함수사용) 다이나믹 프로그래밍 - 피보나치 함수(Fibonacci Function)를 재귀함수로 구현 -> O(N)의 상수 시간 복잡도
+# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화 - 메모이제이션은 탑다운 방식에 국한되어 사용
+d = [0] * 100
+
+
+def fibo1(x):
+    # 종료 조건(1 혹은 2일 때 1을 반환)
+    if x == 1 or x == 2:
+        return 1
+    # 이미 계산한 적 있는 문제라면 그대로 반환
+    if d[x] != 0:
+        return d[x]
+    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+
+print(fibo1(99))
+
+d = [0] * 100
+
+# 바텀업(반복문 사용) 다이나믹 프로그래밍 - 일반적으로 반복문을 이용한 DP가 더 성능이 좋다. DP의 전형적인 방식.
+# 앞서 계산된 결과를 저장하기 위한 'DP 테이블' 초기화
+d = [0] * 100
+
+# 첫 번째 피보나치 수와 두 번째 피보나치 수는 1
+d[1] = 1
+d[2] = 1
+n = 99
+
+# 피보나치 함수(Fibonacci Function) 반복문으로 구현
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+
+print(d[n])
