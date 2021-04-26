13c0664a49b5c55a966af22f1228572fc8fef667
tonyw0527 <tonyw0527@gmail.com>
2021-04-27 05:03:27 +0900

    Update cheatSheet.py - Add dijkstra

--- cheatSheet.py
+++ cheatSheet.py
@@ -1,30 +1,33 @@
 '''
-# Math
+# Combination
 '''
 # Combination
+import sys
+import heapq
 from collections import deque
 from itertools import combinations
 arr = [1, 2, 3, 4, 5, 6, 7]
 cases = list(combinations(arr, 3))  # len(arr) C 3의 조합
 
 '''
-Data Structure
-'''
 # Stack
+'''
 stack = []
 stack.append(1)
 stack.pop()
 
+'''
 # Queue
+'''
 queue = deque()
 queue.append(1)
 queue.popleft()
 list(queue)
 
-'''
-# Algorithm
+
 '''
 # Recusive function
+'''
 
 
 def recursive_function(n):
@@ -35,7 +38,10 @@ def recursive_function(n):
 
 recursive_function(1)
 
-# Depth First Search - stack 자료구조를 기반으로함. -> 따라서 재귀 함수를 이용하여 간결하게 구현.
+
+'''
+# DFS (Depth First Search - stack 자료구조를 기반으로함. -> 따라서 재귀 함수를 이용하여 간결하게 구현.)
+'''
 
 
 def dfs(graph, v, visited):
@@ -64,7 +70,10 @@ visited = [False] * 9  # 노드의 방문 정보. 노드의 개수는 8개이다
 
 dfs(graph, 1, visited)
 
-# Breadth First Search - queue 자료구조 기반. O(N)의 시간 복잡도. 일반적인 경우 실제 수행 시간은 DFS보다 좋은 편.
+
+'''
+# BFS (Breadth First Search - queue 자료구조 기반. O(N)의 시간 복잡도. 일반적인 경우 실제 수행 시간은 DFS보다 좋은 편.)
+'''
 
 
 def bfs(graph, start, visited):
@@ -100,9 +109,11 @@ visited = [False] * 9  # 노드의 방문 정보. 노드의 개수는 8개이다
 
 bfs(graph, 1, visited)
 
-# Dynamic Programming
+'''
+# DP (Dynamic Programming)
+'''
 
-# 피보나치 함수(Fibonacci Function)을 재귀함수로 구현 -> O(2^N)의 지수 시간 복잡도...
+# ex) 피보나치 함수(Fibonacci Function)을 재귀함수로 구현 -> O(2^N)의 지수 시간 복잡도...
 
 
 def fibo(x):
@@ -148,3 +159,57 @@ for i in range(3, n + 1):
     d[i] = d[i - 1] + d[i - 2]
 
 print(d[n])
+
+'''
+# Dijkstra Shortest Path Algorithm - 다익스트라 최단 경로 알고리즘. 우선순위 큐 이용.
+'''
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:  # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+# 다익스트라 알고리즘을 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
